c23fff8815cd219053da413194f69500f3bc36a3
Alex Bacor <bacor12alex17@gmail.com>
2024-03-12 21:12:44 +0800

    Added cs VMfolder

---
+++ ViewModels/TestModel.cs
@@ -0,0 +1,15 @@
+namespace DeskHubMobile.ViewModels;
+
+public class TestModel : ContentPage
+{
+	public TestModel()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
