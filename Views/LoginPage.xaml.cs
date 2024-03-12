3b9c6285480274b37a482c8a35d24a77683270a1
clownzxy <norwen.penas.21@usjr.edu.ph>
2024-03-13 01:43:14 +0800

    test: testing github features

--- Views/LoginPage.xaml.cs
+++ Views/LoginPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class LoginPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void OnClickedSignUpBtn(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new SignUpPage());
+    }
 }
