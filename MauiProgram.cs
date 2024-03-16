f84e9a7e6a88ee2a57203fe36a8e01091da48faa
Alex Bacor <bacor12alex17@gmail.com>
2024-03-16 19:16:40 +0800

    added fonts

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,6 +13,8 @@ namespace DeskHubMobile
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("myfontbold.ttf", "FontOne");
+                    fonts.AddFont("myfont.ttf", "FontTwo");
                 });
 
 #if DEBUG
